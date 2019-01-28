489365b275b120e6fc435e6956f2649beaa1f808
Basil Meer <basilmeer@gmail.com>
2019-01-28 10:41:37 +0500

    Changed the nextUserId from a constant to a variable

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import chat from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { addUser } from "./actions";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import chat from "./reducers";
 
 // Create a Store by passing in the chat Reducer
 const store = createStore(chat);
 
+// Register ourselves as a user
+store.dispatch(addUser("Me"));
+
 ReactDOM.render(
-  <Provider store={ store }>
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
