fb7cc2e1a6b8c131d421b52c6dc0781949fbda48
Basil Meer <basilmeer@gmail.com>
2019-02-01 18:08:24 +0500

    Improved the Sidebar's design

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+
 const Sidebar = ({ users }) => (
   <aside id="sidebar" className="sidebar">
-    <p class="users-online-text">Users Online:</p>
+    <Typography variant="overline">
+      <p class="users-online-text">Users Online:</p>
+    </Typography>
     <ul>
       {users.map(user => (
-        <li key={user.id} className="user-name">
-          {user.name}
+        <li key={user.id} className="online-user">
+          <div className="online-user-avatar">
+            <Avatar id="user-avatar">{user.name[0]}</Avatar>
+          </div>
+          <div className="online-user-name">
+            <Typography variant="body1">{user.name}</Typography>
+          </div>
         </li>
       ))}
     </ul>
