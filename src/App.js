397d8123c5be4a54c0adde9c8915e0131625395d
Basil Meer <basilmeer@gmail.com>
2019-02-01 08:56:12 +0500

    Cleaning up the layout a bit

--- src/App.js
+++ src/App.js
@@ -7,36 +7,23 @@ import { AddMessage } from "./containers/AddMessage";
 import Grid from "@material-ui/core/Grid";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: "center",
-    color: theme.palette.text.secondary
-  }
-});
-
 // Set up the App component
 class App extends Component {
+  
   // Render a generic layout
   render() {
     return (
       <div>
-        <AppBar position="static">
+        <AppBar position="static" className="appbar">
           <Typography variant="h6" color="inherit">
-            Test
+            React and Redux Chat
           </Typography>
         </AppBar>
         <Grid container spacing={24}>
-          <Grid item sm={3}>
-            <Button variant="contained">Test</Button>
+          <Grid item sm={2}>
             <Sidebar />
           </Grid>
-          <Grid item sm={9}>
+          <Grid item sm={1}>
             <section id="main">
               <MessagesList />
               <AddMessage />
